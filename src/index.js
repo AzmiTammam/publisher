abc4c37018f3513941d92493eea12df952c9341c
AzmiTammam <azmitammam@gmail.com>
2021-04-27 04:21:02 +0300

    Edit Photo problem and scss

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import './scss/style.scss';
 import '@laylazi/bootstrap-rtl/dist/css/bootstrap-rtl.min.css';
-import './css/style.css';
 import 'jquery/dist/jquery.min';
 import 'popper.js/dist/popper.min';
 import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
 import '@fortawesome/fontawesome-free/js/all.min';
-
-
-
 
 $(window).scroll(function () {
     if ($(this).scrollTop() >= 580) {
